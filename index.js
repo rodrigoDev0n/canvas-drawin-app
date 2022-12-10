ed778bb6bac276ceed4ec6a443f2c32c52d3bbd4
Rodrigo Poblete <pobletedev@gmail.com>
2022-12-09 21:32:35 -0300

    ultimos TODOS

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// TODO: Modificar interfaz.
+// TODO: Añadir selector intuitivo.
+
 const canvas = document.getElementById('canvas');
 const paintColors = document.getElementById('colors');
 const selected = document.getElementsByName('colores');
